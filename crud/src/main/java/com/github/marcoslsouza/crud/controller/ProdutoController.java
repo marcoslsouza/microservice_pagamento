a3d037d81f586416df07338abc66b5ad09db0e77
marcoslsouza <marcoslsouzadf@gmail.com>
2022-03-23 02:17:02 -0300

    Ajustes ProdutoController->create

--- crud/src/main/java/com/github/marcoslsouza/crud/controller/ProdutoController.java
+++ crud/src/main/java/com/github/marcoslsouza/crud/controller/ProdutoController.java
@@ -1,7 +1,8 @@
 package com.github.marcoslsouza.crud.controller;
 
-import com.github.marcoslsouza.crud.data.vo.ProdutoVO;
-import com.github.marcoslsouza.crud.service.ProdutoService;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,14 +11,25 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import com.github.marcoslsouza.crud.data.vo.ProdutoVO;
+import com.github.marcoslsouza.crud.service.ProdutoService;
 
 @RestController
 @RequestMapping("/produto")
+@CrossOrigin("*")
 public class ProdutoController {
 
     private final ProdutoService produtoService;
@@ -47,7 +59,7 @@ public class ProdutoController {
         Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection,"nome"));
         Page<ProdutoVO> produtos = this.produtoService.findAll(pageable);
         produtos.stream().forEach(p ->
-                p.add(linkTo(methodOn(ProdutoController.class).findById(p.getId())).withSelfRel())
+        	p.add(linkTo(methodOn(ProdutoController.class).findById(p.getId())).withSelfRel())
         );
         PagedModel<EntityModel<ProdutoVO>> pagedModel = assembler.toModel(produtos);
 
@@ -58,7 +70,7 @@ public class ProdutoController {
     public ResponseEntity<ProdutoVO> create(@RequestBody ProdutoVO produtoVO) {
         ProdutoVO prodVO = this.produtoService.create(produtoVO);
         prodVO.add(linkTo(methodOn(ProdutoController.class).findById(prodVO.getId())).withSelfRel());
-        return ResponseEntity.ok().body(prodVO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(prodVO);
     }
 
     @PutMapping(value = "/{id}", produces = "application/json", consumes = "application/json")
